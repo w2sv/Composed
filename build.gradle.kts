d177ecb9cc77227325549d1d9fe57fc871b132d3
w2sv <zangenbergjanek@googlemail.com>
2024-02-29 17:01:28 +0100

    Remove dokka

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,5 +2,4 @@ plugins {
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.vanniktech.maven.publish) apply false
-    alias(libs.plugins.dokka) apply false
 }
