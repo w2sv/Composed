2f6bd8d21360aac8d712ec5d1eaf881b26234037
w2sv <zangenbergjanek@googlemail.com>
2024-03-11 10:26:02 +0100

    0.0.2-rc2

--- composed/src/test/kotlin/com/w2sv/composed/extensions/ModifierKtTest.kt
+++ composed/src/test/kotlin/com/w2sv/composed/extensions/ModifierKtTest.kt
@@ -1,11 +1,10 @@
-package com.w2sv.composed
+package com.w2sv.composed.extensions
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.padding
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
-import com.w2sv.composed.extensions.thenIf
 import junit.framework.TestCase.assertEquals
 import org.junit.Test
 
