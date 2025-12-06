8023b0e47cfc1703b0ee38cad9d313da3865418b
w2sv <zangenbergjanek@googlemail.com>
2025-12-06 14:26:06 +0100

    Update dependencies

--- composed/build.gradle.kts
+++ composed/build.gradle.kts
@@ -43,6 +43,14 @@ android {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
+    // androidx.compose.ui:ui-test-junit4-android:1.10.0 needs minSdk=23
+    sourceSets {
+        getByName("test") {
+            defaultConfig {
+                minSdk = 23
+            }
+        }
+    }
     publishing {
         singleVariant("release") {
             withSourcesJar()
