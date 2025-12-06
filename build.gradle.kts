4a93e0c8d47bdf3b25a35348f5960e08bae14126
w2sv <zangenbergjanek@googlemail.com>
2025-12-06 16:14:19 +0100

    Remove gradle.properties.version & set version from version.txt

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,3 +12,10 @@ plugins {
 versionCatalogUpdate {
     versionSelector(VersionSelectors.PREFER_STABLE)
 }
+
+// Set version from $ROOT/version.txt, which is created or updated when running the make publish routine
+rootProject.layout.projectDirectory.file("version.txt").asFile.let { versionFile ->
+    if (versionFile.exists()) {
+        version = versionFile.readText().trim()
+    }
+}
